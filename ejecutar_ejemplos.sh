87cab18e804cbc0a403ef86c4f087e22c29b6cbd
beaaedo <beaaedo@ucm.es>
2024-03-07 20:03:26 +0100

    Update ejecutar_ejemplos.sh
    este script ejecuta los ejemplos. TODAVIA NO FUNCIONA.

--- ejecutar_ejemplos.sh
+++ ejecutar_ejemplos.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-set -x
+base_folder="/c/Users/Bea/Documents/curso23-24/tfg/codigo/tfg"
+dzn_folder="$base_folder/ejemplos_dzn"
+results_folder="$base_folder/ejemplos_results"
+mzn_script="$base_folder/satisfaccion.mzn"
+minizinc="/c/Program Files/MiniZinc/minizinc.exe"
 
-json_folder="/c/Users/Bea/Documents/curso23-24/tfg/codigo/tfg/ejemplos_json"
-output_folder="/c/Users/Bea/Documents/curso23-24/tfg/codigo/tfg/ejemplos_dzn"
-python_script="/c/Users/Bea/Documents/curso23-24/tfg/codigo/tfg/dzn_generation.py"
+mkdir -p "$results_folder"
 
-for json_file in "$json_folder"/*.json; do
-    if [ -f "$json_file" ]; then
-        base_name=$(basename -- "$json_file")
-        file_name="${base_name%.*}"
-
-        python "$python_script" "$json_file" > "$output_folder/$file_name.dzn"
-    fi
+for dzn_file in "$dzn_folder"/*.dzn; do
+    # Check if the file exists
+    if [ -e "$dzn_file" ]; then
+        base_name=$(basename "$dzn_file" .dzn)
+        result_file="$results_folder/$base_name.txt"
+        "$minizinc" --solver gecode "$mzn_script" "$dzn_file" -o "$result_file"
+        if [ $? -eq 0 ]; then
+            echo "Success: $base_name"
+        else
+            echo "Error: $base_name"
+        fi
+    fi 
 done
